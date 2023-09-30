d7342cc90da1f7234339d3b8bd7176ef962cb834
Jessica <jessicacristina.recife@gmail.com>
2023-09-30 11:48:20 -0300

    exerc em sala sabado

--- exercicios/para-sala/manipulacao-arq.py
+++ exercicios/para-sala/manipulacao-arq.py
@@ -1,5 +1,7 @@
+import csv
 
-# 'r' = read
+
+''' 'r' = read
 
 with open('./dados.txt', 'r') as arquivos:
     conteudo = arquivos.read()
@@ -13,6 +15,31 @@ with open('./dados.txt', 'r') as arquivos:
 
 # 'w' = write
 
-with open('./dados-write.txt', 'w') as arquivos:
-    arquivos.write("fim de linha")
-    
+with open('./dados-write.txt', 'w') as arquivo:
+    arquivo.write("fim de linha")
+
+
+dados = [['Nome', 'Idade'], ['Alice', 30], ['Bob', 25]]
+
+with open('pessoas.csv', 'w', newline='') as arq_csv:
+    escritor_csv = csv.writer(arq_csv)
+    escritor_csv.writerows(dados)
+
+with open('pessoas.csv', 'r') as arq:
+    leitor_csv = csv.reader(arq)
+    for linha in leitor_csv:
+        print(linha)
+
+'''
+import json
+
+'''
+dados = {'Nome': 'Alice', 'Idade': 30, 'Cidade': 'Recife'}, {'Nome': 'Maria', 'Idade': 15, 'Cidade': 'Cabo'}
+
+with open('dados.json', 'w') as arquivo_json:
+    json.dump(dados, arquivo_json)
+'''
+
+with open('dados.json', 'r') as arquivo_json:
+    dados = json.load(arquivo_json)
+    print(dados)
