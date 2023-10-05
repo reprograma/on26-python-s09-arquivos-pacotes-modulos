cd97edf48303ddb5a19222722b6b25d68669aefc
Jessica <jessicacristina.recife@gmail.com>
2023-10-04 21:42:44 -0300

    exerc casa

--- exercicios/para-casa/main.py
+++ exercicios/para-casa/main.py
@@ -2,14 +2,3 @@ from datetime import datetime
 from datautil import anobissexto, calculoidade, formatardata
 
 data = input("\n Insira uma data no formato dd/mm/aaaa: ")
-data_formatada = formatardata.formatardata(data)
-ano_nascimento = int(data.split("/")[-1])
-
-idade_calculada = calculoidade.calcularidade(data)
-
-if anobissexto.ano_bissexto(int(data.split("/")[-1])):
-    print("\n Ano não bissexto")
-else:
-    print("\n Ano bissexto")
-
-print(f"\n {idade_calculada} anos de idade")
